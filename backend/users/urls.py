b426b9a8024277b817b66e8af734e741e96b34e7
kriteshvanidms <kritesh.vani@crowdbotics.com>
2024-03-11 17:49:37 +0530

    test for forgot password with new template

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from users.views import (
-    user_redirect_view,
-    user_update_view,
-    user_detail_view,
-)
-from .views import SendOTPView, PhoneLoginView, VerifyCodeAPIView, PasswordResetView, PasswordResetConfirmView, UniqueUsernameCheck,UserProfileView,DeleteUserByPhone, Login, SignupwithEmailAndUsername, UserOnboardingView, UserStripeToken
+# from users.views import (
+#     user_redirect_view,
+#     user_update_view,
+#     user_detail_view,
+# )
+
+from .views import (SendOTPView, PhoneLoginView, VerifyCodeAPIView, PasswordResetView,
+                    PasswordResetConfirmView, UniqueUsernameCheck, UserProfileView, DeleteUserByPhone,
+                    Login, SignupwithEmailAndUsername, UserOnboardingView, UserStripeToken)
 
 app_name = "users"
 urlpatterns = [
@@ -13,7 +16,7 @@ urlpatterns = [
     path('send-otp/', SendOTPView.as_view()),
     path('phone-login/', PhoneLoginView.as_view()),
     path('onboarding/', UserOnboardingView.as_view(), name='onboarding'),
-    path("~redirect/", view=user_redirect_view, name="redirect"),
+    # path("~redirect/", view=user_redirect_view, name="redirect"),
     # path("~update/", view=user_update_view, name="update"),
     # path("<str:username>/", view=user_detail_view, name="detail"),
     path('confirm-otp/', VerifyCodeAPIView.as_view(), name='verify'),
@@ -22,10 +25,28 @@ urlpatterns = [
         PasswordResetView.as_view(),
         name="forgot-password",
     ),
+    path('password/reset/confirm/', PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm_api'),
     path("profile/", UserProfileView.as_view(), name="profile"),
-    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm_api'),
     path('delete-user/', DeleteUserByPhone.as_view(), name='delete-user'),
     path('login/', Login.as_view(), name='login'),
     path('username-signup/', SignupwithEmailAndUsername.as_view(), name='signup'),
     path('custom_token/', UserStripeToken.as_view(), name='stripe'),
 ]
+
+
+# from django.contrib.auth.views import (
+#     LogoutView,
+#     PasswordResetView,
+#     PasswordResetDoneView,
+#     PasswordResetConfirmView,
+#     PasswordResetCompleteView
+# )
+
+# urlpatterns += [
+#     ###test forgot password
+#     path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset.html'),name='password-reset'),
+#     path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
+#     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
+#     path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
+# ]
