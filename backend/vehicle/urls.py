5b0b174c9e2e3dddfab0840549d823ae97e26ee9
kriteshvanidms <kritesh.vani@crowdbotics.com>
2024-03-13 16:42:59 +0530

    vehicle auction status

--- backend/vehicle/urls.py
+++ backend/vehicle/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import VehicleAdd, VehicleList, EditVehicle, DeleteImage, DeleteVideo
+from .views import VehicleAdd, VehicleList, EditVehicle, DeleteImage, DeleteVideo, GetVehicleAuctionStatus
 urlpatterns = [
     path('add/', VehicleAdd.as_view(), name='vehicle-add'),
     path('list/', VehicleList.as_view(), name='vehicle-list'),
     path('edit/', EditVehicle.as_view(), name='vehicle-edit'),
     path('delete-image/', DeleteImage.as_view(), name='delete-image'),
     path('delete-video/', DeleteVideo.as_view(), name='delete-video'),
+    path('get-vehicle-auction-status/', GetVehicleAuctionStatus.as_view(), name='get-vehicle-auction-status'),
 ]
